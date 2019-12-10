0d9317abbe5a39153e142e9f5d137ad9154936be
tjxp000a <JessicaPomfret@westfieldgrp.com>
2019-12-10 12:42:38 -0500

    day3 V2

--- Day3/Day3.ps1
+++ Day3/Day3.ps1
@@ -1,71 +1,103 @@
-function Get-AllCoordinates {
+function Get-LineCoordinates {
     param (
         $currentCoord,
         $move
     )
-    $allCoords = @()
 
-    $currentX = $currentCoord.X
-    $currentY = $currentCoord.Y
+    $lineCoord = New-Object -TypeName psobject -Property @{
+        'start' = $currentCoord
+        'end' = $null
+    }
+
+    $newX = $currentCoord.X
+    $newY = $currentCoord.Y
 
     $moveSpaces = [convert]::ToInt32($move.Substring(1,$move.Length-1),10)
 
-    if ($move[0] -eq 'R') {
-        for ($i=0; $i -le $moveSpaces; $i++) {
-            $allCoords += New-Object -TypeName psobject -Property @{
-                'x' = $currentX
-                'y' = ($currentY + $i)
-            }
-        }
+    switch ($move[0]) {
+        'R' {$newX += $moveSpaces}
+        'L' {$newX -= $moveSpaces}
+        'U' {$newY += $moveSpaces}
+        'D' {$newY -= $moveSpaces}
     }
-    elseif ($move[0] -eq 'L') {
-        for ($i=0; $i -le $moveSpaces; $i++) {
-            $allCoords += New-Object -TypeName psobject -Property @{
-                'x' = $currentX
-                'y' = ($currentY - $i)
-            }
-        }
+
+    $lineCoord.end = New-Object -TypeName psobject -Property @{
+        'x' = $newX
+        'y' = $newY
     }
-    elseif ($move[0] -eq 'U') {
-        for ($i=0; $i -le $moveSpaces; $i++) {
-            $allCoords += New-Object -TypeName psobject -Property @{
-                'x' = ($currentX + $i)
-                'y' = ($currentY)
-            }
-        }
-    }
-    elseif ($move[0] -eq 'D') {
-        for ($i=0; $i -le $moveSpaces; $i++) {
-            $allCoords += New-Object -TypeName psobject -Property @{
-                'x' = ($currentX - $i)
-                'y' = ($currentY)
-            }
-        }
-    }
-    $allCoords
+
+    $lineCoord
 }
 
-
-function Get-WireCoordinates {
+function Get-AllLines {
     param (
-        $WireMoves
+        $wireMoves
     )
-    $WireCoord = @()
-    foreach($move in $WireMoves) {
-        if($WireCoord) {
-            $currentOneCoord = $WireCoord[-1]
-        }
-        else {
-            $currentOneCoord = New-Object -TypeName psobject -Property @{
-                'x' = 0
-                'y' = 0
-            }
-        }
-        $WireCoord += Get-AllCoordinates -currentCoord $currentOneCoord -Move $move
+
+    $current = New-Object -TypeName psobject -Property @{
+        'x' = 0
+        'y' = 0
     }
-    $WireCoord
+    $lines = @()
+
+    foreach($move in $wireMoves) {
+        $WireOneCoords = Get-LineCoordinates -currentCoord $current -move $move
+        $current = $WireOneCoords.end
+        $lines += $WireOneCoords
+    }
+
+    return $lines
 }
 
+function get-intersect {
+    param (
+        $lineOne,
+        $lineTwo
+    )
+    #Line AB represented as a1x + b1y = c1
+    $a = New-Object -TypeName psobject -Property @{
+        'x' = $lineOne.start.x
+        'y' = $lineOne.start.y
+    }
+    $b = New-Object -TypeName psobject -Property @{
+        'x' = $lineOne.end.x
+        'y' = $lineOne.end.y
+    }
+    $a1 = $b.y - $a.y
+    $b1 = $a.x - $b.x
+    $c1 = $a1 * $a.x + $b1 * $a.y
+
+    #Line CD represented as a2x + b2y = c2
+    $c = New-Object -TypeName psobject -Property @{
+        'x' = $lineTwo.start.x
+        'y' = $lineTwo.start.y
+    }
+    $d = New-Object -TypeName psobject -Property @{
+        'x' = $lineTwo.end.x
+        'y' = $lineTwo.end.y
+    }
+    $a2 = $d.y - $c.y
+    $b2 = $c.x - $d.x
+    $c2 = $a2 * $c.x + $b2 * $c.y
+
+    $determinant = $a1 * $b2 - $a2 * $b1
+
+    if ($determinant -ne 0)
+    {
+        $x = ($b2 * $c1 - $b1 * $c2) / $determinant;
+        $y = ($a1 * $c2 - $a2 * $c1) / $determinant;
+        # make sure x is on the lines
+        if((($x -ge $a.x -and $x -le $b.x) -or ($x -ge $b.x -and $x -le $a.x)) -and (($x -ge $c.x -and $x -le $d.x) -or ($x -ge $d.x -and $x -le $c.x))) {
+            # make sure y is on the lines
+            if((($y -ge $a.y -and $y -le $b.y) -or ($y -ge $b.y -and $y -le $a.y)) -and (($y -ge $c.y -and $y -le $d.y) -or ($y -ge $d.y -and $y -le $c.y))) {
+                New-Object -TypeName psobject -Property @{
+                    'x' = $x
+                    'y' = $y
+                }
+            }
+        }
+    }
+}
 
 function Get-ManhattanDistance {
     param (
@@ -73,8 +105,8 @@ function Get-ManhattanDistance {
     )
 
     foreach ($c in $coords) {
-        if($c.X -ne 0 -and $c.Y -ne 0) {
-            $mh = [math]::abs($c.X) + [math]::abs($c.Y)
+        if($c.X -ne 0 -or $c.Y -ne 0) {
+            $mh = [math]::abs((0 - [math]::abs($c.X)) + (0 - [math]::abs($c.Y)))
             New-Object -TypeName psobject -Property @{
                 'Coord' = $c
                 'ManhattanDistance' = $mh
@@ -83,18 +115,17 @@ function Get-ManhattanDistance {
     }
 }
 
-#$wireOne = "R8","U5","L5","D3"
-#$wireTwo = "U7","R6","D4","L4"
-
 $wireOne = (get-content .\Day3\Wire1.txt) -split (',')
 $wireTwo = (get-content .\Day3\Wire2.txt) -split (',')
 
-$WireOneCoords = Get-WireCoordinates -WireMoves $wireOne | Select * -Unique | Sort-Object
-$WireTwoCoords = Get-WireCoordinates -WireMoves $wireTwo | Select * -Unique | Sort-Object
+$wireOneLines = Get-AllLines -wireMoves $wireOne
+$wireTwoLines = Get-AllLines -wireMoves $wireTwo
 
-$crosses = Compare-Object -DifferenceObject  $WireOneCoords -ReferenceObject $WireTwoCoords -IncludeEqual -Property y,x -ExcludeDifferent -PassThru | select x,y
+$crosses = @()
+foreach ($lineOne in $wireOneLines) {
+    foreach($linetwo in $wireTwoLines) {
+        $crosses += get-intersect -lineOne $lineOne -lineTwo $lineTwo
+    }
+}
 
-Get-ManhattanDistance -coords $crosses | sort ManhattanDistance
-
-
-
+Get-ManhattanDistance -coords $crosses | sort ManhattanDistance | select -first 1
