f26aca2de71dbd7ccbc1f40c9a06ea02110c345b
jpomfret <jpomfret7@gmail.com>
2019-12-26 10:38:05 -0500

    day4 updates

--- Day4.ps1
+++ Day4.ps1
@@ -28,22 +28,77 @@ function Get-DigitIncrease {
     )
     # Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
     $codeArray = $code -split '' | Where-Object { $_ }
-    for($i = 1; $i -le $codeArray.Count; $i++){
-        if($codeArray[$i+1] -and ($codeArray[$i+1] -ge $codeArray[$i])) {
-            Return $True
+    $good = $true
+    for($i = 0; $i -le $codeArray.Count-1; $i++){
+        if($codeArray[$i+1] -and ($codeArray[$i] -gt $codeArray[$i+1])) {
+            $good = $false
         }
     }
+    return $good
 }
+
 
 $puzzleInput = @{
     "Low"  = 171309
     "High" = 643603
 }
-$counter = 0
+
+<#
+$Stepone = @()
 for ($i = $puzzleInput.Low; $i -le $puzzleInput.High; $i++) {
-    if((Get-Double -Code $i) -and (Get-DigitIncrease -code $i)) {
+    if(Get-DigitIncrease -code $i) {
         $i
-        $counter ++
+        $stepOne += $i
     }
 }
-$counter
+
+
+$code = $puzzleInput.Low
+
+$codeArray = $code -split '' | Where-Object { $_ }
+
+$codeArray
+#>
+<#
+function Set-Digit {
+    param (
+        $code,
+        $digit,
+        $value
+    )
+    $codeArray = $code -split '' | Where-Object { $_ }
+    $codeArray[$digit] = $value
+
+    return $codeArray -join ''
+
+}
+#>
+
+
+# Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
+$code = $puzzleInput.Low
+$Stepone = @()
+while ($code -lt $puzzleInput.High) {
+    if($code -le $puzzleInput.High) {
+        $codeArray = $code -split '' | Where-Object { $_ }
+
+        if ($codeArray[1..5] -join '' -eq '99999') {
+            $codeArray[0] = [int32]$codeArray[0] + 1
+            $code = [int32]$code + 1
+        }
+
+        for($i = 1; $i -le $codeArray.Count-1; $i++){
+            $i
+            if ($codeArray[$i] -lt $codeArray[$i+1]) {
+                $codeArray[$i]
+                $codeArray[$i] = [int32]$codeArray[$i] + 1
+            }
+        }
+
+    }
+    $code = $codeArray -join ''
+    if (Get-DigitIncrease -Code $code) {
+        $code
+        $stepOne += $code
+    }
+}
